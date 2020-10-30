89bf26b61c84d46e48be353e21b0d853dae95054
Omichougule <chougule.omkar10@gmail.com>
2020-10-30 11:28:30 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+        $("#mycarousel").carousel( { interval: 2000, pause: false } );
+            $("#carouselButton").click(function(){
+                if($('#carouselButton').children('span').hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $('#carouselButton').children('span').removeClass('fa-pause');
+                    $('#carouselButton').children('span').addClass('fa-play');
+                }
+                else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+                    $("#mycarousel").carousel('cycle');
+                    $('#carouselButton').children('span').removeClass('fa-play');
+                    $('#carouselButton').children('span').addClass('fa-pause');
+                }
+                
+            });
+    
+
+
+        $('#login').click(function(){
+            $('#loginModal').modal('toggle');
+        });
+        $('#reservetable').click(function(){
+            $('#reserveTableModal').modal('toggle');
+        });
+   
